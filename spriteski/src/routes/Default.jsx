e261eead06b24119b4d3ac895d1c4beb81a70741
LesPantsFancy <mjnrock@umich.edu>
2023-04-07 14:15:26 -0400

    init

--- spriteski/src/routes/Default.jsx
+++ spriteski/src/routes/Default.jsx
@@ -1,12 +1,11 @@
 import { useState, useRef, useEffect } from "react";
-import { Base64 } from "./../modules/tiles/Base64.js";
 
 import { InputText } from "../components/InputField.jsx";
 
 import { Tile } from "./../modules/tiles/Tile.js";
 import { TileSet } from "./../modules/tiles/TileSet.js";
 
-export function CanvasComponent({ canvas }) {
+export function CanvasComponent({ canvas, ...props }) {
 	const canvasRef = useRef(null);
 
 	useEffect(() => {
@@ -21,6 +20,7 @@ export function CanvasComponent({ canvas }) {
 		<canvas
 			ref={ canvasRef }
 			className={ `p-2 border border-gray-200 hover:border-gray-300 border-solid rounded shadow-lg cursor-pointer` }
+			{ ...props }
 		/>
 	);
 }
@@ -29,7 +29,8 @@ export function Default() {
 	const [ image, setImage ] = useState(null);
 	const [ tileWidth, setTileWidth ] = useState(32);
 	const [ tileHeight, setTileHeight ] = useState(32);
-	const [ resultColumns, setResultColumns ] = useState(1);
+	const [ resultMagnitude, setResultMagnitude ] = useState(1);
+	const [ isRowXCol, setIsRowXCol ] = useState(true);
 	const [ tiles, setTiles ] = useState([]);
 	const canvasRef = useRef(null);
 
@@ -52,29 +53,114 @@ export function Default() {
 		document.getElementById("fileInput").click();
 	};
 	const calculateRowsAndColumns = (type = 0) => {
-		let columns = 1;
-
+		let threshold = 1;
 		if(type === 0) {
-			columns = Math.ceil(Math.sqrt(tiles.length));
+			threshold = Math.ceil(Math.sqrt(tiles.length));
 		} else if(type === 1) {
-			columns = Math.ceil(image.width / tileWidth);
+			threshold = Math.ceil(image.width / tileWidth);
+		} else if(type === 2) {
+			threshold = Math.ceil(image.height / tileHeight);
 		}
 
-		setResultColumns(columns);
+		setResultMagnitude(threshold);
+	};
+	const renderTiles = () => {
+		let result;
+
+		if(isRowXCol) {
+			result = tiles
+				// LTR-TTB
+				.reduce((rows, tile, index) => {
+					const rowIndex = Math.floor(index / resultMagnitude);
+					const columnIndex = index % resultMagnitude;
+
+					if(!rows[ rowIndex ]) {
+						rows[ rowIndex ] = [];
+					}
+
+					rows[ rowIndex ][ columnIndex ] = (
+						<CanvasComponent key={ tile.id } canvas={ tile.canvas } />
+					);
+
+					return rows;
+				}, []);
+		} else {
+			result = tiles
+				// TTB-LTR
+				.reduce((rows, tile, index) => {
+					const rowIndex = index % resultMagnitude;
+					const columnIndex = Math.floor(index / resultMagnitude);
+
+					if(!rows[ rowIndex ]) {
+						rows[ rowIndex ] = [];
+					}
+
+					rows[ rowIndex ][ columnIndex ] = (
+						<CanvasComponent key={ tile.id } canvas={ tile.canvas } />
+					);
+
+					return rows;
+				}, []);
+		}
+
+
+		return result.map((row, index) => (
+			<div key={ index } className={ `flex  gap-2` }>
+				{ row }
+			</div>
+		));
+	};
+	const tessellate = async e => {
+		const tileset = await TileSet.Factory({ source: canvasRef.current });
+
+		if(isRowXCol) {
+			// LTR-TTB
+			for(let row = 0; row < tileset.height; row += tileHeight) {
+				for(let col = 0; col < tileset.width; col += tileWidth) {
+					const tile = new Tile({ width: tileWidth, height: tileHeight });
+
+					await tile.paint(tileset.canvas, col, row, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
+
+					tileset.addTile(tile);
+				}
+			}
+		} else {
+			// TTB-LTR
+			for(let col = 0; col < tileset.width; col += tileWidth) {
+				for(let row = 0; row < tileset.height; row += tileHeight) {
+					const tile = new Tile({ width: tileWidth, height: tileHeight });
+
+					await tile.paint(tileset.canvas, col, row, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
+
+					tileset.addTile(tile);
+				}
+			}
+		}
+
+		canvasRef.current = tileset.canvas;
+
+		setTiles(tileset.tiles);
+
+		calculateRowsAndColumns(isRowXCol ? 1 : 2);
 	};
 
 	useEffect(() => {
-		const canvas = canvasRef.current;
-		const context = canvas.getContext("2d");
 		if(image) {
-			canvas.width = image.width;
-			canvas.height = image.height;
+			const context = canvasRef.current.getContext("2d");
+			canvasRef.current.width = image.width;
+			canvasRef.current.height = image.height;
 			context.drawImage(image, 0, 0);
 		}
 	}, [ image ]);
 
+	useEffect(() => {
+		if(image) {
+			setTiles([ ...tiles ]);
+		}
+	}, [ isRowXCol ]);
+
 	return (
-		<div className="flex flex-col h-screen gap-4 m-4">
+		<div className="flex flex-col h-screen gap-4 m-8">
 			<div className="m-auto">
 				<input
 					type="file"
@@ -102,30 +188,9 @@ export function Default() {
 			<hr />
 
 			<div className="m-auto">
-				<div className="flex flex-col w-full gap-2">
+				<div className={ `flex flex-col w-full gap-2` }>
 					{
-						tiles
-							.reduce((rows, tile, index) => {
-								const rowIndex = Math.floor(index / resultColumns);
-								const columnIndex = index % resultColumns;
-
-								if(!rows[ rowIndex ]) {
-									rows[ rowIndex ] = [];
-								}
-
-								rows[ rowIndex ][ columnIndex ] = (
-									<div key={ tile.id } className="flex-none">
-										<CanvasComponent canvas={ tile.canvas } />
-									</div>
-								);
-
-								return rows;
-							}, [])
-							.map((row, index) => (
-								<div key={ index } className="flex gap-2">
-									{ row }
-								</div>
-							))
+						renderTiles()
 					}
 				</div>
 			</div>
@@ -152,25 +217,7 @@ export function Default() {
 
 							<button
 								className="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-								onClick={ async e => {
-									const tileset = await TileSet.Factory({ source: canvasRef.current });
-
-									for(let y = 0; y < tileset.height; y += tileHeight) {
-										for(let x = 0; x < tileset.width; x += tileWidth) {
-											const tile = new Tile({ width: tileWidth, height: tileHeight });
-
-											await tile.paint(tileset.canvas, x, y, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
-
-											tileset.addTile(tile);
-										}
-									}
-
-									canvasRef.current = tileset.canvas;
-
-									setTiles(tileset.tiles);
-
-									calculateRowsAndColumns(1);
-								} }
+								onClick={ tessellate }
 								style={ {
 									fontFamily: "'Fredoka One', cursive"
 								} }
@@ -188,10 +235,10 @@ export function Default() {
 							<div className="flex flex-row w-full gap-4">
 								<InputText
 									type="number"
-									label="Display Columns"
+									label={ `Display Size` }
 									placeholder="1, 2, 3, ..."
-									value={ resultColumns }
-									onChange={ e => setResultColumns(~~e.target.value) }
+									value={ resultMagnitude }
+									onChange={ e => setResultMagnitude(~~e.target.value) }
 								/>
 
 								<button
@@ -206,6 +253,16 @@ export function Default() {
 									onClick={ e => calculateRowsAndColumns(1) }
 								>
 									Source Ratio
+								</button>
+
+								<button
+									className="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+									onClick={ e => {
+
+										setIsRowXCol(!isRowXCol);
+									} }
+								>
+									Swap to { isRowXCol ? "TTB-LTR" : "LTR-TTB" }
 								</button>
 							</div>
 						</div>
