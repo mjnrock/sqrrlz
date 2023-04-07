b9c3c8220a40a9ce3941d538721aac3e2665282e
LesPantsFancy <mjnrock@umich.edu>
2023-04-07 18:00:46 -0400

    init

--- spriteski/src/App.jsx
+++ spriteski/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Default from "./routes/Default";
 import Tessellator from "./routes/Tessellator";
